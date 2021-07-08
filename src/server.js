9a3e545a106ca9c19bf165857f3b9056e24c20cf
yat98 <hidayatchandra08@gmail.com>
2021-07-08 17:33:36 +0800

    Menambahkan hapi server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const { routes } = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        host: (process.env.NODE_ENV !== 'production') ? 'localhost' : process.env.APP_PROD_URL,
+        port: process.env.APP_PORT,
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan ${server.info.uri}`);
+};
+
+init();
