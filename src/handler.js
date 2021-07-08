9042b24a7ae34af69893c0059455ec295dd5855e
yat98 <hidayatchandra08@gmail.com>
2021-07-08 19:36:56 +0800

    Menambahkan validasi addBookHandler

--- src/handler.js
+++ src/handler.js
@@ -13,9 +13,28 @@ const addBookHandler = (request, h) => {
         reading,
     } = request.payload;
 
+    if (!name) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }
+
+    if (readPage > pageCount) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    }
+
     const id = nanoid(16);
     const insertedAt = new Date().toString();
     const updatedAt = insertedAt;
+    const finished = pageCount === readPage;
 
     const newBook = {
         id,
@@ -26,6 +45,7 @@ const addBookHandler = (request, h) => {
         publisher,
         pageCount,
         readPage,
+        finished,
         reading,
         insertedAt,
         updatedAt,
