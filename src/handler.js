3ced31c1a8958a1837f486f580e70d28d174b3f9
yat98 <hidayatchandra08@gmail.com>
2021-07-08 21:38:41 +0800

    Menambahkan getBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -85,4 +85,25 @@ const getAllBooksHandler = () => {
     };
 };
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const book = books.filter((b) => b.id === id)[0];
+
+    if (book !== undefined) {
+        const response = h.response({
+            status: 'success',
+            data: { book },
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
