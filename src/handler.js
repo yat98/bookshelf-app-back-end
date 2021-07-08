3c12ce80c4bfeac235f12004f9411550e80dee14
yat98 <hidayatchandra08@gmail.com>
2021-07-08 19:20:16 +0800

    Menambahkan addBookHandler

---
+++ src/handler.js
@@ -0,0 +1,58 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+    } = request.payload;
+
+    const id = nanoid(16);
+    const insertedAt = new Date().toString();
+    const updatedAt = insertedAt;
+
+    const newBook = {
+        id,
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+        insertedAt,
+        updatedAt,
+    };
+
+    books.push(newBook);
+
+    const isSuccess = books.some((book) => book.id === id);
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil ditambahkan',
+            data: {
+                bookId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
+};
+
+module.exports = { addBookHandler };
