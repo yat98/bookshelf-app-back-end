2f0845c8d6994abac0e471d37885fba1ba798642
yat98 <hidayatchandra08@gmail.com>
2021-07-08 17:35:20 +0800

    Memperbaiki routes import

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-const { routes } = require('./routes');
+const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
